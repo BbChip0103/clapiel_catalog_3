6238d167a9a0416fbdc98b6f005170a1c1ab9553
BbChip <bbchip13@gmail.com>
2018-01-13 13:50:06 +0900

    Add sidebar menu

--- js/custom_script.js
+++ js/custom_script.js
@@ -10,10 +10,41 @@ $(function($) {
   }
 });
 
+$(function(){
+	var container = $('#st-container'),
+            	reset = $('#st-trigger-effects'),
+    resetMenu = function() {
+      $('.st-container.st-effect').removeClass('st-menu-open');
+      $('.burger-toggler').toggleClass('show');
+      $('.icon-bar').toggleClass('show');
 
-$('.burger-toggler').click(function () {
-  $('.burger-toggler').toggleClass('show');
-  $('.icon-bar').toggleClass('show');
-  $('.navbar').stop(true, true).fadeToggle();
-  // $('.navbar').stop(true, true).slideToggle();
+      $(document).unbind( "click", bodyClickFn );
+      $(document).unbind( "click", resetClickFn );
+    },
+		bodyClickFn = function(evt) {
+      if( $(evt.target).parents('.st-menu').length || $(evt.target).hasClass('st-menu') ) {
+			} else {
+        resetMenu();
+      }
+		},
+		resetClickFn = function(evt) {
+			if (evt.target == reset) {
+				resetMenu();
+			}
+		};
+
+    $('#st-trigger-effects').click(function (ev) {
+      ev.stopPropagation();
+      ev.preventDefault();
+
+      $('.burger-toggler').toggleClass('show');
+      $('.icon-bar').toggleClass('show');
+
+      setTimeout( function() {
+				$('.st-container.st-effect').addClass('st-menu-open');
+			}, 50 );
+
+      $(document).bind( "click", bodyClickFn );
+      $(document).bind( "click", resetClickFn );
+    });
 });
